445df676ca7c43e68b6b31fc4025cc1f781b8986
Alán F. Muñoz <afer.mg@gmail.com>
2024-09-01 16:23:46 -0400

    fix: works on darwin

--- machines/darwin001/default.nix
+++ machines/darwin001/default.nix
@@ -1,19 +1,117 @@
+{ config, pkgs, inputs, outputs, lib, ... }:
+let user = "alan"; in
 {
-  config,
-  pkgs,
-  inputs,
-  outputs,
-  ...
-}: {
+
   imports = [
+    #../../modules/darwin/home-manager.nix
+    # ../../modules/shared
+    # ../../modules/shared/cachix
     inputs.home-manager.darwinModules.home-manager
     inputs.nix-homebrew.darwinModules.nix-homebrew
     ../common/darwin_home_manager.nix
-    (import ../common/nix-homebrew.nix { inherit inputs; user = "alan";})
+    (import ../common/nix-homebrew.nix { inherit inputs; user = "${user}";})
     ../common/nix.nix
     ../common/substituters.nix
+    
   ];
 
+
+  services = {nix-daemon.enable = true; # Auto upgrade nix package and the daemon service.
+              tailscale.enable = true; # Network of devices
+  };
+  
+
+  # Setup user, packages, programs
+  # nix = {
+  #   # package = pkgs.nix;
+  #   settings.trusted-users = [ "@admin" "${user}" ];
+
+  #   gc = {
+  #     user = "root";
+  #     automatic = true;
+  #     interval = { Weekday = 0; Hour = 2; Minute = 0; };
+  #     options = "--delete-older-than 30d";
+  #   };
+
+  #   # Turn this on to make command line easier
+  #   extraOptions = ''
+  #     experimental-features = nix-command flakes
+  #   '';
+  # };
+
+  # Turn off NIX_PATH warnings now that we're using flakes
+  system.checks.verifyNixPath = false;
+
+  fonts = {
+     packages = [ pkgs.dejavu_fonts pkgs.iosevka pkgs.nerdfonts ];
+   };
+
+  # Load configuration that is shared across systems
+  environment.systemPackages = with pkgs; [
+    emacs
+    # (emacs-unstable-pgtk.override({
+    #   withX = false;
+    #   withXwidgets=false;
+    #   withTreeSitter=true;
+    #   withNS=false;
+    #   withGTK3=true;
+    #   withPgtk=true;
+    # }))
+    (pkgs.writeShellScriptBin "glibtool" "exec ${pkgs.libtool}/bin/libtool $@")
+  ] ++ (import ../../modules/shared/packages.nix { inherit pkgs; });
+  # (import ../../modules/shared/packages.nix { inherit pkgs; });
+
+  launchd.user.agents.emacs.path = [ config.environment.systemPath ];
+  launchd.user.agents.emacs.serviceConfig = {
+    KeepAlive = true;
+    ProgramArguments = [
+      "/bin/sh"
+      "-c"
+      "/bin/wait4path ${pkgs.emacs}/bin/emacs && exec ${pkgs.emacs}/bin/emacs --fg-daemon"
+    ];
+    StandardErrorPath = "/tmp/emacs.err.log";
+    StandardOutPath = "/tmp/emacs.out.log";
+  };
+
+    system = {
+  # Turn off NIX_PATH warnings now that we're using flakes
+    stateVersion = 4;
+
+    defaults = {
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        ApplePressAndHoldEnabled = false;
+
+        # 120, 90, 60, 30, 12, 6, 2
+        KeyRepeat = 2;
+
+        # 120, 94, 68, 35, 25, 15
+        InitialKeyRepeat = 15;
+
+        "com.apple.mouse.tapBehavior" = 1;
+        "com.apple.sound.beep.volume" = 0.0;
+        "com.apple.sound.beep.feedback" = 0;
+      };
+
+      dock = {
+        autohide = true;
+        show-recents = false;
+        launchanim = true;
+        orientation = "left";
+        tilesize = 48;
+      };
+
+      finder = {
+        _FXShowPosixPathInTitle = false;
+      };
+
+      trackpad = {
+        Clicking = true;
+        TrackpadThreeFingerDrag = true;
+      };
+    };
+  };
+  
   # Configure nixpkgs
   nixpkgs = {
     # You can add overlays here
@@ -26,12 +124,13 @@
   };
 
   # Create users
-  users.users.alan = {
+  users.users.${user} = {
     description = "Alan Munoz";
-    home = "/Users/alan";
+  
+    home = "/Users/${user}";
     createHome = true;
     isHidden = false;
-    initialPassword = "password";
+    #initialPassword = "password";
     shell = pkgs.fish;
     # openssh.authorizedKeys.keyFiles = [
     #   ../../homes/ank/id_rsa.pub
@@ -39,9 +138,6 @@
     # ];
   };
 
-  # This is important! Removing this will break your shell and thus your system
-  # This is needed even if you enable zsh in home manager
-  programs.zsh.enable = true;
 
   # Configure homebrew
   homebrew = {
@@ -63,11 +159,18 @@
       # Look into why enabling this break shell for starship
       # useUserPackages = true;
       extraSpecialArgs = {inherit inputs outputs;};
-      users.alan = {
+      users.${user}= {
         imports = [
-          ../../homes/alan/machines/darwin001.nix
+          ../../homes/amunoz/darwin001.nix
         ];
+      programs = {
+        # This is important! Removing this will break your shell and thus your system
+        # This is needed even if you enable zsh in home manager
+        zsh.enable = true;
+        fish.enable = true;
+      } // import ../../modules/shared/home-manager.nix { inherit config pkgs lib; };
       };
+    backupFileExtension = "bak";
     };
 
   # sudo with touch id
@@ -80,45 +183,6 @@
   # Disable press and hold for diacritics.
   # I want to be able to press and hold j and k
   # in vim to move around.
-  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
+  # system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
 
-  # Turn off NIX_PATH warnings now that we're using flakes
-  system.checks.verifyNixPath = false;
-  system = {
-    stateVersion = 4;
-
-    #   defaults = {
-    #     NSGlobalDomain = {
-    #       AppleShowAllExtensions = true;
-    #       ApplePressAndHoldEnabled = false;
-    #
-    #       # 120, 90, 60, 30, 12, 6, 2
-    #       KeyRepeat = 2;
-    #
-    #       # 120, 94, 68, 35, 25, 15
-    #       InitialKeyRepeat = 15;
-    #
-    #       "com.apple.mouse.tapBehavior" = 1;
-    #       "com.apple.sound.beep.volume" = 0.0;
-    #       "com.apple.sound.beep.feedback" = 0;
-    #     };
-    #
-    #     dock = {
-    #       autohide = true;
-    #       show-recents = false;
-    #       launchanim = true;
-    #       orientation = "left";
-    #       tilesize = 48;
-    #     };
-    #
-    #     finder = {
-    #       _FXShowPosixPathInTitle = false;
-    #     };
-    #
-    #     trackpad = {
-    #       Clicking = true;
-    #       TrackpadThreeFingerDrag = true;
-    #     };
-    #   };
-  };
 }
---
+++ modules/shared/home-manager.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, lib, ... }:
+
+let name = "Alán F. Muñoz";
+    user = "alan";
+    email = "afer.mg@gmail.com"; in
+{
+  # Shared shell configuration
+
+  git = {
+    enable = true;
+    ignores = [ "*.swp" ];
+    userName = name;
+    userEmail = email;
+    lfs = {
+      enable = true;
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+      core = {
+	    editor = "emacs";
+        autocrlf = "input";
+      };
+      pull.rebase = true;
+      rebase.autoStash = true;
+    };
+  };
+
+  ssh = {
+    enable = true;
+    includes = [
+      (lib.mkIf pkgs.stdenv.hostPlatform.isLinux
+        "/home/${user}/.ssh/config_external"
+      )
+      (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
+        "/Users/${user}/.ssh/config_external"
+      )
+    ];
+    matchBlocks = {
+      "github.com" = {
+        identitiesOnly = true;
+        identityFile = [
+          (lib.mkIf pkgs.stdenv.hostPlatform.isLinux
+            "/home/${user}/.ssh/id_github"
+          )
+          (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
+            "/Users/${user}/.ssh/id_github"
+          )
+        ];
+      };
+    };
+  };
+
+}
