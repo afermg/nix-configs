5d1b65f68fe14ca34ae55aa78bad2a8a5d40ab33
Alán F. Muñoz <afer.mg@gmail.com>
2024-09-29 22:23:27 -0400

    feat(nix): add java zulu17

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -5,4 +5,6 @@ let
   shared-packages = import ../shared/packages.nix { inherit pkgs; };
 in shared-packages ++
    ( import ../shared/media_server.nix { inherit pkgs; } ) ++
-   [ dockutil ]
+   [ dockutil
+     zulu17
+   ]
