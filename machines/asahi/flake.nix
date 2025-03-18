ee30acc8a607f810751e19f05843c5e10829400f
Alan Munoz <afer.mg@gmail.com>
2025-03-17 21:19:25 -0400

    new: add asahi linux (fedora) minimal home-manager+flakes config

---
+++ machines/asahi/flake.nix
@@ -0,0 +1,30 @@
+# flake.nix
+{
+  description = "My Home Manager configuration";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+
+    home-manager = {
+    url = "github:nix-community/home-manager/release-24.11";
+    inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ...}:
+    let
+      lib = nixpkgs.lib;
+      system = "aarch64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+	config.allowUnfree = true;
+ 	};
+    in {
+
+      homeConfigurations = {
+        myprofile = home-manager.lib.homeManagerConfiguration {
+	  inherit pkgs;
+	  modules = [ ./home.nix ];
+	};
+      };
+    };
+}
---
+++ machines/asahi/home.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "alan";
+  home.homeDirectory = "/home/alan";
+
+  # Packages that should be installed to the user profile.
+  home.packages = (pkgs.callPackage ../../modules/shared/packages.nix {});
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Alan Munoz";
+    userEmail = "afer.mg@gmail.com";
+    #extraConfig = {
+    # Sign all commits using ssh key
+    #    commit.gpgsign = true;
+    #    gpg.format = "ssh";
+    #    gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+    #    user.signingkey = "~/.ssh/id_ed25519.pub";
+    #  };
+  };
+
+
+  programs.emacs = {
+    enable = true;
+    extraPackages = epkgs: [
+     epkgs.nix-mode
+     epkgs.magit
+    ];
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
+
+}
