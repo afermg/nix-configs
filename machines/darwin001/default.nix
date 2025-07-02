5b5c97254e4c24ca72da5625c472b40b3494fc7f
Alán F. Muñoz <amunozgo@broadinstitute.org>
2025-07-02 17:08:14 -0400

    nix(dock): adjust directory to display Downloads

--- machines/darwin001/default.nix
+++ machines/darwin001/default.nix
@@ -202,7 +202,7 @@ in
         options = "--sort name --view grid --display folder";
       }
       {
-        path = "${config.users.users.${user}.home}/.local/share/downloads";
+        path = "${config.users.users.${user}.home}/Downloads";
         section = "others";
         options = "--sort name --view grid --display stack";
       }
