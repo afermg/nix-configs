097d5d51cd93c941d9d86b2a5361553c07723570
Alán F. Muñoz <afer.mg@gmail.com>
2025-09-11 17:49:47 -0400

    add machine key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,9 @@
 let
   # personal_key = "ssh-rsa AAAA....";
-  moby_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKdcdlNS1SO+rJHjRQWd33qvqBEZcZR8ypTQUeC9LZ4 amunozgo@broadinstitute.org ";
+  personal_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKdcdlNS1SO+rJHjRQWd33qvqBEZcZR8ypTQUeC9LZ4 amunozgo@broadinstitute.org ";
+  moby_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIClOuXVukvwqgE+UDxJShus+JGprTC8QIoc1G/Ege5KK";
   keys = [
-    # personal_key
+    personal_key
     moby_key
   ];
 in
