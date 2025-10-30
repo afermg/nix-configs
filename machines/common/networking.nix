2d7da28e9c670f1ab7b7521e4163edc0cffb94b6
Alán F. Muñoz <afer.mg@gmail.com>
2025-10-30 15:48:59 -0400

    fix(networking): add resolved service for DNS, fixes tailscale overwriting /etc/resolv.conf

--- machines/common/networking.nix
+++ machines/common/networking.nix
@@ -2,5 +2,11 @@
 {
   networking.networkmanager.enable = true;
   networking.firewall.enable = false;
+
+  networking.nameservers = [
+    "1.1.1.1#one.one.one.one"
+    "1.0.0.1#one.one.one.one"
+  ];
+
   systemd.services.NetworkManager-wait-online.enable = false;
 }
--- machines/moby/default.nix
+++ machines/moby/default.nix
@@ -40,6 +40,19 @@
   programs.nix-ld.enable = true;
 
   services = {
+    # systemd-resolved for DNS
+    resolved = {
+      enable = true;
+      dnssec = "true";
+      domains = [ "~." ];
+      fallbackDns = [
+        "1.1.1.1#one.one.one.one"
+        "1.0.0.1#one.one.one.one"
+      ];
+      dnsovertls = "true";
+    };
+
+    # Desktop components
     desktopManager.gnome.enable = true;
     displayManager.gdm = {
       enable = true;
