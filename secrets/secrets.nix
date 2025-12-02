7ddf6d9b4fd315a4bf844d6ecc2cda911839e214
Alán F. Muñoz <afer.mg@gmail.com>
2025-12-02 16:36:27 -0500

    chore(secrets): Remove email from personal key
    Removes the email annotation and trailing whitespace from the SSH
    public key string.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   # personal_key = "ssh-rsa AAAA....";
-  personal_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKdcdlNS1SO+rJHjRQWd33qvqBEZcZR8ypTQUeC9LZ4 amunozgo@broadinstitute.org ";
+  personal_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKdcdlNS1SO+rJHjRQWd33qvqBEZcZR8ypTQUeC9LZ4";
   moby_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIClOuXVukvwqgE+UDxJShus+JGprTC8QIoc1G/Ege5KK";
   keys = [
     personal_key
