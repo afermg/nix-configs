87607bb153b155d901d0668e8b205d6581ccff65
Alán F. Muñoz <afer.mg@gmail.com>
2026-05-01 18:58:11 -0400

    chore(moby): Disable Steam
    The Steam configuration, previously used for Blender, has been
    commented out to disable it on the moby machine.

--- machines/moby/default.nix
+++ machines/moby/default.nix
@@ -156,12 +156,12 @@
     fish.enable = true;
 
     # For blender
-    steam = {
-      enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    };
+    # steam = {
+    #   enable = true;
+    #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    #   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    # };
   };
 
   # Networking
