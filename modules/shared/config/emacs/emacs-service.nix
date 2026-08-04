95089b433da119126d89a451fb77f69bb56fbacb
Alán F. Muñoz <afer.mg@gmail.com>
2026-08-04 11:54:32 -0400

    fix(emacs): install client binaries in the home profile

--- modules/shared/config/emacs/emacs-service.nix
+++ modules/shared/config/emacs/emacs-service.nix
@@ -1,14 +1,19 @@
 { lib, pkgs, ... }:
+let
+  emacsPackage = pkgs.emacs.override {
+    withImageMagick = true;
+    withXwidgets = false; # https://github.com/nix-community/emacs-overlay/issues/466
+  };
+in
 {
+  programs.emacs = {
+    enable = true;
+    package = emacsPackage;
+  };
+
   services.emacs = {
     enable = true;
     startWithUserSession = true;
-    package =
-      (pkgs.emacs.override {
-        withImageMagick = true;
-        withXwidgets = false; # https://github.com/nix-community/emacs-overlay/issues/466
-      }).pkgs.withPackages
-        (_epkgs: [ ]);
   };
 
   systemd.user.services.emacs = lib.mkIf pkgs.stdenv.isLinux {
